3371caccc788badacc3c95c969d8db512276e86a
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-07 11:32:10 +0300

    reviews

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,16 +1,18 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
-      def index
-        airline = Airline.all
+      protect_from_forgery with: :null_session
 
-        render json: AirlineSerializer.new(airlines).serialized_json
+      def index
+        airlines = Airline.all
+
+        render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airline).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def create
@@ -27,7 +29,7 @@ module Api
         airline = Airline.find_by(slug: params[:slug])
 
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airline).serialized_json
+          render json: AirlineSerializer.new(airline, options).serialized_json
         else
           render json: { error: airline.errors.messages }, status: 422
         end
@@ -47,6 +49,10 @@ module Api
 
       def airline_params
         params.require(:airline).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= { include: %i[reviews]}
       end
     end
   end
