1dc4e8018101f169eed6e5f5e77054f8f082e937
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-10 10:52:43 +0300

    Review

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect } from 'react'
+import React, {useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Header from './Header'
+import ReviewForm from './ReviewForm'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -41,23 +42,62 @@ const Airline = (props) => {
     .catch( resp => console.log(resp) )
   }, [])
 
+  const handleChange = (e) => {
+    e.preventDefault()
+
+    setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+
+    console.log('review:', review)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const csrfToken = document.querySelector('[name=csrf-token]').content
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+    const airline_id = airline.data.id
+    axios.post('/api/v1/reviews', {review, airline_id})
+    .then(resp => {
+      const included = [...airline.included, resp.data]
+      setAirline({...airline, included})
+      setReview({title: '', description: '', score: 0})
+    })
+    .catch(resp => {})
+  }
+
+  const setRating = (score, e) => {
+    e.preventDefault
+
+    setReview({...review, score})
+  }
+
   return (
     <Wrapper>
-      <Column>
-        <Main>
-          {
-            loaded &&
-            <Header
+      {
+        loaded &&
+        <Fragment>
+          <Column>
+            <Main>
+
+                <Header
+                  attributes={airline.data.attributes}
+                  reviews={airline.included}
+                />
+              <div className="reviews"></div>
+            </Main>
+          </Column>
+          <Column>
+            <ReviewForm
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              setRating={setRating}
               attributes={airline.data.attributes}
-              reviews={airline.included}
+              review={review}
             />
-          }
-          <div className="reviews"></div>
-        </Main>
-      </Column>
-      <Column>
-        <div className="review-form">[Review Form Goes Here.]</div>
-      </Column>
+          </Column>
+        </Fragment>
+      }
     </Wrapper>
   )
 }
